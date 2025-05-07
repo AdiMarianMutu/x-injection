4cc73cd84c1b4d3b8b1ae5645646b3360633182e
Adi-Marian Mutu <mutu.adi.marian@gmail.com>
2025-05-07 13:49:48 +0300

    fix: add the `name` property to the `XInjectionProviderModuleDisposedError` class

--- src/errors/provider-module-disposed.error.ts
+++ src/errors/provider-module-disposed.error.ts
@@ -2,6 +2,8 @@ import { IProviderModule } from '../types';
 import { XInjectionProviderModuleError } from './provider-module.error';
 
 export class XInjectionProviderModuleDisposedError extends XInjectionProviderModuleError {
+  name = XInjectionProviderModuleDisposedError.name;
+
   constructor(module: IProviderModule) {
     super(module, 'Has been disposed! You can re-initialize it by using the `_lazyInit` method.');
   }

d6fde752e8c7da27f5a8dbcc256d9a4ca7ee2e58
Adi-Marian Mutu <mutu.adi.marian@gmail.com>
2025-05-07 21:45:19 +0300

    fix(type): remove leftover code

--- src/types/provider-module/provider-module-options.ts
+++ src/types/provider-module/provider-module-options.ts
@@ -1,14 +1,12 @@
 import type { Container } from 'inversify';
 
-import type { ContainerStrategy, InjectionScope } from '../../enums';
+import type { InjectionScope } from '../../enums';
 import type { ProviderToken } from '../provider-token';
 import type { IProviderModule } from './provider-module';
 
 export interface ProviderModuleOptions {
   /** The module unique ID. */
   identifier?: symbol;
-
-  containerStrategy?: ContainerStrategy;
 
   /** The list of imported {@link IProviderModule | modules} that export the {@link Provider | providers} which are required in this module. */
   imports?: IProviderModule[];
